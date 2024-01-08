30e9e9490b16bd5872c963318b75f269ae60c556
garold664 <garold664@gmail.com>
2024-01-08 14:23:06 +0300

    added more photos popup

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -5,13 +5,17 @@ import { useParams } from 'react-router-dom';
 export default function PlacePage() {
   const { id } = useParams();
   const [place, setPlace] = useState(null);
+
+  //! 5:22
+
+  const [showAllPhotos, setShowAllPhotos] = useState(false);
+
   useEffect(() => {
     if (!id) return;
     axios.get(`/places/${id}`).then((res) => {
       setPlace(res.data);
     });
   }, [id]);
-  if (!place) return;
 
   const placePhoto = (index) => {
     return (
@@ -25,21 +29,108 @@ export default function PlacePage() {
     );
   };
 
+  if (!place) return;
+
+  //! 5:20
+  if (showAllPhotos) {
+    return (
+      <div className="absolute bg-blue-200 inset-0 min-h-screen">
+        <div className="p-8 grid gap-4">
+          <div>
+            <h2 className="text-3xl">Photos of {place.title}</h2>
+            <button
+              onClick={() => setShowAllPhotos(false)}
+              className="fixed right-2 top-2 flex gap-1 p-2 rounded-md bg-gray-200 shadow-lg shadow-gray-900 duration-150 hover:shadow-md hover:shadow-gray-700"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+              Close photos
+            </button>
+          </div>
+          {place?.photos?.length > 0 &&
+            place.photos.map((photo) => (
+              <div>
+                <img
+                  className="object-cover aspect-square w-full"
+                  src={`http://localhost:4000/uploads/${photo}`}
+                  alt=""
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="mt-4 bg-gray-200 -mx-8 px-8 py-8">
       <h1 className="text-3xl">{place.title}</h1>
       <a
-        className="block font-semibold my-2 underline underline-offset-4"
+        className="flex gap-2 items-center font-semibold my-2 underline underline-offset-4"
         target="_blank"
         href={`https://maps.google.com/?q=${place}`}
       >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-4 h-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+          />
+        </svg>
+
         {place.address}
+      </a>
+      <div className="relative">
         <section className="grid gap-2 grid-cols-[2fr_1fr] grid-rows-2">
           <div className="row-span-2">{placePhoto(0)}</div>
           <div className="">{placePhoto(1)}</div>
           <div className="">{placePhoto(2)}</div>
         </section>
-      </a>
+        <button
+          onClick={() => setShowAllPhotos(true)}
+          className="flex absolute bottom-2 right-2 py-2 px-4 rounded-xl border-2 border-black bg-white shadow-md shadow-gray-900 transition duration-300 hover:scale-105 hover:bg-gray-200"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 mr-2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"
+            />
+          </svg>
+          Show more photos
+        </button>
+      </div>
     </div>
   );
 }
