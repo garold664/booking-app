b9f073dcff2691207802a163b8c84ea025102a15
garold664 <garold664@gmail.com>
2024-06-19 17:48:25 +0300

    saved client baseUrl in env variable

--- client/src/baseUrl.ts
+++ client/src/baseUrl.ts
@@ -1,4 +1,6 @@
-const baseUrl = 'http://localhost:4000';
+// const baseUrl = 'http://localhost:4000';
+const baseUrl = import.meta.env.VITE_BASE_URL;
+console.log('baseUrl: ', baseUrl);
 // const baseUrl = 'https://booking-app-api-wpom.onrender.com';
 
 export default baseUrl;
