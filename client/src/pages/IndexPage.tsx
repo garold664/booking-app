54a7a246b584a5e54be026d19574f5a5d6a3602c
garold664 <garold664@gmail.com>
2024-06-19 15:42:09 +0300

    added error and loading states on IndexPage

--- client/src/pages/IndexPage.tsx
+++ client/src/pages/IndexPage.tsx
@@ -2,22 +2,39 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { PlaceType } from '../../../lib/types';
+import { PlaceType } from '../lib/types';
 import baseUrl from '../baseUrl';
 
 export default function IndexPage() {
   const [places, setPlaces] = useState<PlaceType[] | []>([]);
+  const [error, setError] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    axios.get('/places').then((response) => {
-      setPlaces([...response.data]);
-    });
+    setIsLoading(true);
+    axios
+      .get('/places')
+      .then((response) => {
+        setPlaces([...response.data]);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setError(error.message);
+      });
     return () => {};
   }, []);
 
   return (
     <div className="container mx-auto grid grid-cols-2 gap-x-6 gap-y-8 md:grid-cols-3 lg:grid-cols-6 mt-8">
-      {places.length > 0 &&
+      {!isLoading && error !== '' && <h1>{error}</h1>}
+      {!isLoading && error === '' && places.length === 0 && <h1>No places</h1>}
+      {isLoading && error === '' && places.length > 0 && (
+        <h1 className="text-center">Loading...</h1>
+      )}
+      {!isLoading &&
+        error === '' &&
+        places.length > 0 &&
         places.map((place: PlaceType) => (
           <Link to={`/place/${place._id}`} key={place._id}>
             {place.photos?.[0] && (
